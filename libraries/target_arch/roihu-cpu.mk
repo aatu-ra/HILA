42e95fa4c1d5b55d8e68fb762186c6b36fa88a07
aatu-ra <aatu.rajala@gmail.com>
2026-09-25 16:37:43 +0300

    roihu target_arches added

---
+++ libraries/target_arch/roihu-cpu.mk
@@ -0,0 +1,57 @@
+# Platform specific makefile for vanilla (linux) mpi code 
+#
+# this is included from main.mk -file, which is in turn included from 
+# application makefile
+#
+#
+
+$(info ########################################################################)
+$(info Target roihu-cpu: default modules seem to work )
+$(info Building hilapp on roihu: )
+$(info     > cd HILA_DIR/singularity )
+$(info     > singularity build hilapp.sif hilapp_local.def )
+$(info     > mv hilapp.sif HILA/hilapp/bin/hilapp )
+$(info ########################################################################)
+
+
+### Define compiler and options
+
+# c++ standard level, can be set in makefile or command line
+CPPSTD := c++17
+
+# Define compiler
+CC := mpic++
+LD := mpic++
+
+# Define compilation flags
+CXXFLAGS  := -O3 -x c++ --std=$(CPPSTD) -fno-rtti -mavx2 -mfma -march=native
+CXXFLAGS_NOOPT := -x c++ --std=$(CPPSTD) -fno-rtti
+#CXXFLAGS := -g -x c++ --std=c++17
+
+# hilapp needs to know where c++ system include files are located.  This is not a problem if
+# hilapp was built from system installed clang, but if hilapp was statically compiled elsewhere
+# and copied here it must be told.  Instead of hunting the directories by hand, we can ask
+# system installed compilers.  g++ should be present almost everywhere.  The strange incantation
+# below makes g++ list the search directories.  The result is written to build/0hilapp_incl_dirs
+
+# HILAPP_INCLUDE_LIST := $(addprefix -I, $(shell echo | g++ -xc++ --std=c++17 -Wp,-v - 2>&1 | grep "^ /"))
+
+
+# Write hilapp inlcudes to a file 0hilapp_incl_dirs
+$(shell mkdir -p build)
+# $(shell echo "$(HILAPP_INCLUDE_LIST)" > build/0hilapp_incl_dirs )
+# HILAPP_INCLUDES := `cat build/0hilapp_incl_dirs`
+
+
+# Linker libraries and possible options
+
+LDLIBS  := -lfftw3 -lfftw3f -lm
+LDFLAGS :=
+
+# These variables must be defined here
+#
+HILAPP_OPTS := $(HILAPP_INCLUDES)
+HILA_OPTS := -DNODE_LAYOUT_BLOCK=128
+
+
+
